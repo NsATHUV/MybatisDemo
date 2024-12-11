862bdd3cf3416072f3a47b9573392420821b25ed
NsATS <nsathuv@qq.com>
2024-12-11 11:07:38 +0800

    规范接口

--- src/main/java/com/xh/dao/UserDao.java
+++ src/main/java/com/xh/dao/UserDao.java
@@ -11,21 +11,21 @@ public interface UserDao {
      * @param userBean 传入UserBean对象。
      * @return 大于1表示成功，0 失败
      */
-    public int addUser(UserBean userBean);
+    int addUser(UserBean userBean);
 
     /**
      * 更新用户信息
      *
      * @param userBean 传入UserBean对象。
      */
-    public void updateUser(UserBean userBean);
+    void updateUser(UserBean userBean);
 
     /**
      * 根据ID删除用户信息
      *
      * @param id 传入id
      */
-    public void deleteUser(int id);
+    void deleteUser(int id);
 
     /**
      * 根据ID查询用户的信息
@@ -33,12 +33,12 @@ public interface UserDao {
      * @param id 传入用户的ID
      * @return 用户对象
      */
-    public UserBean getUserById(int id);
+    UserBean getUserById(int id);
 
     /**
      * 获取所有用户的信息
      *
      * @return 存储所有用户对象的List集合
      */
-    public List<UserBean> getUserAll();
+     List<UserBean> getUserAll();
 }
